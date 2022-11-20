730222e3c451a6b0cd7cf7070f8eb80aff3e0b2e
Saraf Nawar <s.999895831@gmail.com>
2022-11-19 19:11:43 -0500

    https

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//testing if this file will save 
+getRandomMeal();
+//the below function should post a random recipe in the middle of the homepage
+
+
+async function getRandomMeal(){
+    var resp =await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    var randomMeal =await resp.json();
+    console.log(randomMeal);
+    
+}
+
+//this function will look up full meals by their ID (replaced i= with +id)
+async function getMealById(id){
+    var meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' +id); 
+}
+
+//set up the functions first and then add the variables needed 
+//the below function will search meals by their name (replaced the s= with +term)
+async function getMealsBySearch(term){
+    var meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' +term);
+}
+
+
+//look up the API categories to name the functions 
